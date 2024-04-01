2de7f448b7f0c59a4f178cb2de4041ba5e58f820
Abdul Hanan Sharif <abd.hanangill@gmail.com>
2024-04-01 13:58:00 +0500

    authentication and post form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['name'=>$incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+        }
+
+        return redirect('/');
+    }
+    public function logout(Request $request){
+    //    Auth::logout();
+       auth()->logout();
+        return redirect('/');
+    }
     public function register(Request $request){
         $incomingFields = $request->validate([
-            'name' => ['required', 'min:3', 'max:30'],
-            'email' =>['required', 'email'],
+            'name' => ['required', 'min:3', 'max:30', Rule::unique('users', 'name')],
+            'email' =>['required', 'email', Rule::unique('users', 'email')],
             'password' => ['required', 'min:5', 'max:25'],
         ]);
 
         $incomingFields['password'] = bcrypt($incomingFields['password']); //hashing password
-        User::create($incomingFields); // create a new user
+        $user = User::create($incomingFields); // create a new user
+        auth()->login($user); // login
 
-        return "Hello from our controller";
+        return redirect('/');
     }
 }
