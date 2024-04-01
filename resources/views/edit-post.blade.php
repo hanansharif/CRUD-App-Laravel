2817ff0ba6f900f79659fbbef246aa82d33bd6ae
Abdul Hanan Sharif <abd.hanangill@gmail.com>
2024-04-01 19:13:34 +0500

    Final Commit

---
+++ resources/views/edit-post.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container my-5 p-5 rounded-4" style="background-color: beige">
+        <h2 style="text-align: center">Edit Post</h2>
+        <form action="/edit-post/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}">
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Post Body</label>
+                <textarea class="form-control" name="body" id="body" rows="3" placeholder="Body Content ...">{{ $post->body }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Save Changes</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
